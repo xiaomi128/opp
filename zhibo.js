fe1abe9a8854f90831434335cd1521cd88d0bffd
xiaomi128 <114792601+xiaomi128@users.noreply.github.com>
2023-06-19 21:17:08 +0800

    Add files via upload

--- zhibo.js
+++ zhibo.js
@@ -23,7 +23,7 @@ const VERSION = 'live2cms 20230616';
 const UA = 'Mozilla/5.0'; //默认请求ua
 const __ext = {data_dict:{}};
 const tips = `\n道长直播转点播js-当前版本${VERSION}`;
-const def_pic = 'https://jihulab.com/susu88/ztv/-/raw/master/tu/88.png';
+const def_pic = 'https://agit.ai/susu123/ztv/raw/branch/main/sp/99.jpg';
 /**
  * 打印日志
  * @param any 任意变量
